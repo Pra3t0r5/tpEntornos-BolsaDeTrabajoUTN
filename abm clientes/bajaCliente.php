0a96f9adc6916d3785f5884725114a1c57ea2a8d
Lucas Pavan <lucaspavan.lp@gmail.com>
2019-04-30 00:46:44 -0300

    ABM Clientes (no terminado)

---
+++ abm clientes/bajaCliente.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Baja Clientes</title>
+    </head>
+    <body>
+        <?php
+        include("Conexion.php");
+        //Primero traigo los datos del form
+        ?>
+    </body>
+</html>
---
+++ abm clientes/modificaCliente.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Modifica Cliente</title>
+    </head>
+    <body>
+        <?php
+        include("Conexion.php");
+        
+        ?>
+    </body>
+</html>
